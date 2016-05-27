7f531787563e9edf0694f3bd0994d9fff01c6ae7
handioq <ter.alexanderandr@gmail.com>
2016-05-27 23:13:37 +0300

    check some data from CurrentWeather

--- app/src/main/java/com/github/handioq/weatherapp/MainActivity.java
+++ app/src/main/java/com/github/handioq/weatherapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.github.handioq.weatherapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.survivingwithandroid.weather.lib.WeatherClient;
@@ -20,7 +24,9 @@ import com.survivingwithandroid.weather.lib.client.okhttp.WeatherDefaultClient;
 import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
 import com.survivingwithandroid.weather.lib.exception.WeatherProviderInstantiationException;
 import com.survivingwithandroid.weather.lib.model.City;
+import com.survivingwithandroid.weather.lib.model.CurrentWeather;
 import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
+import com.survivingwithandroid.weather.lib.request.WeatherRequest;
 
 import java.util.List;
 
@@ -28,6 +34,17 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView textView2;
+    private String testCityID;
+    private TextView textView4;
+
+    private TextView cityText;
+    private TextView condDescr;
+    private TextView temp;
+    private TextView hum;
+    private TextView press;
+    private TextView windSpeed;
+    private TextView windDeg;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +54,16 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         textView2 = (TextView) findViewById(R.id.textView2);
+        textView4 = (TextView) findViewById(R.id.textView4);
+
+        cityText = (TextView) findViewById(R.id.cityText);
+        condDescr = (TextView) findViewById(R.id.condDescr);
+        temp = (TextView) findViewById(R.id.temp);
+        hum = (TextView) findViewById(R.id.hum);
+        press = (TextView) findViewById(R.id.press);
+        windSpeed = (TextView) findViewById(R.id.windSpeed);
+        windDeg = (TextView) findViewById(R.id.windDeg);
+        imgView = (ImageView) findViewById(R.id.imgView);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -79,13 +106,12 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
-
-
         client.searchCity("Minsk,BY", new WeatherClient.CityEventListener() {
             @Override
             public void onCityListRetrieved(List<City> cityList) {
                 // When the data is ready you can implement your logic here
-                textView2.setText(cityList.get(0).getName());
+                textView2.setText(cityList.get(0).getName() + " | id: " + cityList.get(0).getId());
+                testCityID = cityList.get(0).getId();
             }
 
             @Override
@@ -97,6 +123,40 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onConnectionError(Throwable t) {
                 textView2.setText("onConnectionError");
+            }
+        });
+
+        client.getCurrentCondition(new WeatherRequest(testCityID), new WeatherClient.WeatherEventListener() {
+            @Override public void onWeatherRetrieved(CurrentWeather currentWeather) {
+                float currentTemp = currentWeather.weather.temperature.getTemp();
+                Log.d("WL", "City ["+currentWeather.weather.location.getCity()+"] Current temp ["+currentTemp+"]");
+                textView4.setText("Current temp [" + currentTemp + "]");
+
+                //currentWeather.weather.iconData = ((new WeatherDefaultClient()).getImage(currentWeather.weather.currentCondition.getIcon()));
+
+                if (currentWeather.weather.iconData != null && currentWeather.weather.iconData.length > 0) {
+                    Bitmap img = BitmapFactory.decodeByteArray(currentWeather.weather.iconData, 0, currentWeather.weather.iconData.length);
+                    imgView.setImageBitmap(img);
+                }
+
+                cityText.setText(currentWeather.weather.location.getCity() + "," + currentWeather.weather.location.getCountry());
+                condDescr.setText(currentWeather.weather.currentCondition.getCondition() + "(" + currentWeather.weather.currentCondition.getDescr() + ")");
+                temp.setText("" + Math.round((currentWeather.weather.temperature.getTemp())) + "C");
+                hum.setText("" + currentWeather.weather.currentCondition.getHumidity() + "%");
+                press.setText("" + currentWeather.weather.currentCondition.getPressure() + " hPa");
+                windSpeed.setText("" + currentWeather.weather.wind.getSpeed() + " mps");
+                windDeg.setText("" + currentWeather.weather.wind.getDeg() + "");
+
+            }
+
+            @Override public void onWeatherError(WeatherLibException e) {
+                Log.d("WL", "Weather Error - parsing data");
+                e.printStackTrace();
+            }
+
+            @Override public void onConnectionError(Throwable throwable) {
+                Log.d("WL", "Connection error");
+                throwable.printStackTrace();
             }
         });
 
